9a19ba39693aa78bb4d00c6affafb67078d5c75c
sgnsgn <sgnsgn@proton.me>
2024-07-02 13:08:31 +0200

    Update: Add Disclaimer component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,15 +2,34 @@
 import Casino from "@/components/Casino";
 import NotConnected from "@/components/NotConnected";
 import SlotMachine from "@/components/SlotMachine";
-
+import Disclaimer from "@/components/Disclaimer";
 import { useAccount } from "wagmi";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [disclaimerAccepted, setDisclaimerAccepted] = useState(false);
+
+  useEffect(() => {
+    const isAccepted = localStorage.getItem("disclaimerAccepted");
+    if (isAccepted === "true") {
+      setDisclaimerAccepted(true);
+    }
+  }, []);
+
+  const handleAcceptDisclaimer = () => {
+    setDisclaimerAccepted(true);
+    localStorage.setItem("disclaimerAccepted", "true");
+  };
+
   // const { address, isConnected } = useAccount();
   // return <>{isConnected ? <Casino address={address} /> : <NotConnected />}</>;
   return (
     <>
-      <SlotMachine />
+      {!disclaimerAccepted ? (
+        <Disclaimer onAccept={handleAcceptDisclaimer} />
+      ) : (
+        <SlotMachine />
+      )}
     </>
   );
 }
