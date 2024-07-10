f68c081fb3c1f77cb15f701702cf463bee7be166
sgnsgn <sgnsgn@proton.me>
2024-07-10 18:45:38 +0200

    Checkpoint: custom RPC, sepolia.js

--- frontend/app/CustomRainbowKitProvider.jsx
+++ frontend/app/CustomRainbowKitProvider.jsx
@@ -6,13 +6,14 @@ import {
   darkTheme,
 } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
-import { sepolia, hardhat } from "wagmi/chains";
+// import { sepolia, hardhat } from "wagmi/chains";
+import { sepolia } from "@/utils/sepolia";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const config = getDefaultConfig({
   appName: "My Alyra's Final Project App",
   projectId: "57363625597bd2fe4ea8f702650b4b0d",
-  chains: [sepolia, hardhat],
+  chains: [sepolia],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
