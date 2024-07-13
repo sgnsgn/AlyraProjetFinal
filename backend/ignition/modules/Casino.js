74a70908c31fbdcd3265f9efb9c1e4ddd3ea89aa
sgnsgn <sgnsgn@proton.me>
2024-07-14 01:34:52 +0200

    Update

--- backend/ignition/modules/Casino.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("LockModule", (m) => {
-  const casino = m.contract("Casino");
-
-  return { casino };
-});
